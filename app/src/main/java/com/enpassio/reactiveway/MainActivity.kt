e270c861a74fbf44c6fda59b3b5da2d99294d796
Greta Gri <greta.klimaite@gmail.com>
2018-09-05 21:17:55 +0300

    Example 5: Custom Data Type, Operators

--- app/src/main/java/com/enpassio/reactiveway/MainActivity.kt
+++ app/src/main/java/com/enpassio/reactiveway/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.enpassio.reactiveway
 
 import android.os.Bundle
+import android.support.v4.app.FragmentActivity
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import io.reactivex.Observable
@@ -9,6 +10,12 @@ import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.functions.Predicate
 import io.reactivex.observers.DisposableObserver
 import io.reactivex.schedulers.Schedulers
+import io.reactivex.internal.disposables.DisposableHelper.isDisposed
+import android.provider.ContactsContract.CommonDataKinds.Note
+import io.reactivex.ObservableEmitter
+import io.reactivex.ObservableOnSubscribe
+
+
 
 
 class MainActivity : AppCompatActivity() {
@@ -17,11 +24,11 @@ class MainActivity : AppCompatActivity() {
      * from source: https://www.androidhive.info/RxJava/tutorials/
      * https://www.androidhive.info/RxJava/android-getting-started-with-reactive-programming/
      *
-     * Basic Observable, Observer, Subscriber, Disposable, Operators example 1, 2, 3, 4
-     * Observable emits list of animal names
-     * Added Disposable
-     * Added operator - filter()
-     * Added CompositeDisposable
+     * Basic Observable, Observer, Subscriber, Disposable, Operators, CompositeDisposable and
+     * DisposableObservercustom data type - example 1, 2, 3, 4, 5
+     *
+     * The observable emits custom data type (Note) instead of primitive data types
+     * .map() operator is used to turn the note into all uppercase letters
      */
 
     companion object {
@@ -34,90 +41,63 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //Observable
-        val animalsObservable = getAnimalsObservable()
-
-        //Observer with filter letter b
-        val animalsObserver = getAnimalsObserver()
-
-        //Observer with filter letter c + all caps
-        val animalsObserverAllCaps = getAnimalsAllCapsObserver()
-
-
-        //Observer subscribing to observable
-        // filter() is used to filter out the animal names starting with `b`
-        compositeDisposable.add(animalsObservable
-                //subscribeOn(Schedulers.io()): This tell the Observable to run the task on a
-                // background thread.
+        // add to Composite observable
+        // .map() operator is used to turn the note into all uppercase letters
+        compositeDisposable.add(getNotesObservable()
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                // observeOn(AndroidSchedulers.mainThread()): This tells the Observer to receive
-                // the data on Android UI thread so that you can take any UI related actions.
-                //filter() operator filters the data by applying a conditional statement. The data
-                // which meets the condition will be emitted and the remaining will be ignored.
-                .filter {s: String -> s.toLowerCase().startsWith("b")}
-                .subscribeWith(animalsObserver))
-
-        // filter() is used to filter out the animal names starting with `c`
-        //map() is used to transform all the characters to UPPER case
-        compositeDisposable.add(animalsObservable.subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .filter {s: String -> s.toLowerCase().startsWith("c")}
-                .map{s: String-> s.toUpperCase()}
-                .subscribeWith(animalsObserverAllCaps))
+                // Making the note to all uppercase
+                .map{note:Note -> Note(note.id, note.note.toUpperCase())}
+                .subscribeWith(getNotesObserver()))
     }
 
 
-    private fun getAnimalsObservable(): Observable<String> {
-        return Observable.fromArray(
-                "Ant", "Ape",
-                "Bat", "Bee", "Bear", "Butterfly",
-                "Cat", "Crab", "Cod",
-                "Dog", "Dove",
-                "Fox", "Frog");
-    }
-
-    private fun getAnimalsObserver(): DisposableObserver<String> {
-        return object : DisposableObserver<String>() {
-
+    private fun getNotesObserver(): DisposableObserver<Note> {
+        return object : DisposableObserver<Note>() {
             //onNext(): This method will be called when Observable starts emitting the data.
-            override fun onNext(s: String) {
-                Log.d(TAG, "Name: $s")
+            override fun onNext(note: Note) {
+                Log.d(TAG, "Note: " + note.note)
             }
-
             //onError(): In case of any error, onError() method will be called.
             override fun onError(e: Throwable) {
                 Log.e(TAG, "onError: " + e.message)
             }
-
             //onComplete(): When an Observable completes the emission of all the items,
             // onCompleted() will be called.
             override fun onComplete() {
-                Log.d(TAG, "All items are emitted!")
+                Log.d(TAG, "All notes are emitted!")
             }
         }
     }
 
-    private fun getAnimalsAllCapsObserver(): DisposableObserver<String> {
-        return object : DisposableObserver<String>() {
 
-            //onNext(): This method will be called when Observable starts emitting the data.
-            override fun onNext(s: String) {
-                Log.d(TAG, "Name: $s")
+    private fun getNotesObservable(): Observable<Note> {
+        val notes = prepareNotes()
+
+        return Observable.create { emitter ->
+            for (note in notes) {
+                if (!emitter.isDisposed) {
+                    emitter.onNext(note)
+                }
             }
 
-            //onError(): In case of any error, onError() method will be called.
-            override fun onError(e: Throwable) {
-                Log.e(TAG, "onError: " + e.message)
-            }
-
-            //onComplete(): When an Observable completes the emission of all the items,
-            // onCompleted() will be called.
-            override fun onComplete() {
-                Log.d(TAG, "All items are emitted!")
+            if (!emitter.isDisposed) {
+                emitter.onComplete()
             }
         }
     }
+
+    private fun prepareNotes(): List<Note> {
+        val notes = mutableListOf<Note>()
+        notes.add(Note(1, "buy tooth paste!"))
+        notes.add(Note(2, "call brother!"))
+        notes.add(Note(3, "watch narcos tonight!"))
+        notes.add(Note(4, "pay power bill!"))
+
+        return notes
+    }
+
+    internal data class Note(var id: Int, var note: String)
 
     override fun onDestroy() {
         super.onDestroy()
