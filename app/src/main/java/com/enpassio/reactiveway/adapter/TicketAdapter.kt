a8b29e8229820ee8c4ed8a2317f2f2bdc4619f1f
Greta Gri <greta.klimaite@gmail.com>
2018-09-26 00:36:08 +0300

    Big part of tutorial done, still need to fix main activity - not perfect AS performance converting to Kotlin :)

---
+++ app/src/main/java/com/enpassio/reactiveway/adapter/TicketAdapter.kt
@@ -0,0 +1,5 @@
+package com.enpassio.reactiveway.adapter
+
+/**
+ * Created by Greta Grigutė on 2018-09-25.
+ */
---
+++ app/src/main/java/com/enpassio/reactiveway/network/model/Ticket.kt
@@ -0,0 +1,5 @@
+package com.enpassio.reactiveway.network.model
+
+/**
+ * Created by Greta Grigutė on 2018-09-25.
+ */
